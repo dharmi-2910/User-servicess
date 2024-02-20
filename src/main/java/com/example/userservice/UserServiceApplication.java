05f8b7a7a15beeebf5b9bef86b9a15223169a905
abc <abc2@gmail.com>
2024-02-20 20:34:21 +0530

    user commeted

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,6 +1,5 @@
 package com.example.userservice;
 
-import com.example.userservice.entities.Rating;
 import com.example.userservice.external.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -15,17 +14,8 @@ public class UserServiceApplication {
 
     void contextLoad(){
     }
-
     @Autowired
     private RatingService ratingService;
-
-//    @Test
-//    void createRating(){
-//       Rating rating = Rating.builder().rating(String.valueOf(7)).userId(1).hotel(1).feedback("this is created using feign client").build();
-//        Rating ratingResponseEntity = ratingService.createRating(rating);
-//        ratingResponseEntity.
-//           System.out.println("new rating created");
-//    }
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
