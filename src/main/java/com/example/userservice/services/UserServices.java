c50eae9e64f1a8a031670b934faeb67b4c51707b
abc <abc2@gmail.com>
2024-02-29 12:25:02 +0530

    commeted updated

--- src/main/java/com/example/userservice/services/UserServices.java
+++ src/main/java/com/example/userservice/services/UserServices.java
@@ -1,15 +1,11 @@
 package com.example.userservice.services;
 
-import com.example.userservice.UserServiceApplication;
 import com.example.userservice.entities.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public  interface UserServices {
-     Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
      User saveUser(User user);
     List<User> getAllUser();
     User getUser(int userId);
